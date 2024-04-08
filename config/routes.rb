77fa2502751f93f26cdc127b6a874233a12bb673
penny271 <hirotaka.aoki27@gmail.com>
2024-04-09 08:13:54 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  # root "application#hello"
+  root "static_pages#home"  # * root_path, root_url が作成される
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
 end
