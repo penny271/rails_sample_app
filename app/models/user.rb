acd6e6fa4944b201cabe319e50d0572f814d8869
penny271 <hirotaka.aoki27@gmail.com>
2024-04-12 09:34:22 +0900

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # * ユーザーをデータベースに保存する前にemail属性を強制的に小文字に変換する
+  # * これにより、大文字と小文字を区別しない一意性が保証される
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! } # * この行は上の行と同じ意味 直接変更することができる
+  # validates ${:attribute}, presence: true
+  # validates(:name, presence: true)
+  # validates('name', 'email', {presence: true}) # * 文字列でもシンボルでもOK
+  validates(:name, {presence: true})
+  validates :name, length: { maximum: 50 }  # * 文字列でもシンボルでもOK
+  # validates :email, length: { maximum: 255 }  # * 文字列でもシンボルでもOK
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+                    # uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
+
+# /Users/aokihirotaka/Documents/_programming/web_materials/ruby_on_rails_tutorial/sample_app/app/models/user.rb
---
+++ test/models/user_test.rb
@@ -0,0 +1,92 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    # * has_secure_passwordを使って新しいカラムに値が必要なため、passwordとpassword_confirmationを追加
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    # assert_notメソッドを使って Userオブジェクトが有効でなくなったことを確認します
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    # assert_notメソッドを使って Userオブジェクトが有効でなくなったことを確認します
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    # * 通常、メールアドレスでは大文字小文字が区別されません。
+    # * すなわち、foo@bar.comはFOO@BAR.COMやFoO@BAr.coMと書いても扱いは同じです。
+    # duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
+
+# このコードは、Ruby on Railsのモデルテストを定義しています。具体的には、Userモデルのテストを行っています。
+
+# まず、test_helperをrequireしています。test_helperは、テストを行うための各種設定やヘルパーメソッドを提供します。これにより、テストコードの中でRailsの各種機能を利用することができます。
+
+# 次に、UserTestクラスを定義しています。このクラスはActiveSupport::TestCaseを継承しており、これによりRailsのテストフレームワークの機能を利用することができます。
+
+# setupメソッドでは、テストを行う前の準備を行っています。ここでは新しいUserインスタンスを作成し、それを@userインスタンス変数に格納しています。この@userインスタンス変数は、後続のテストメソッドで使用されます。
+
+# 最後に、"should be valid"というテストを定義しています。このテストでは、@userが有効（valid）であることを確認しています。assertメソッドは、引数が真であることを確認します。つまり、@user.valid?がtrueを返すことを期待しています。もし@user.valid?がfalseを返すと、このテストは失敗します。
+
+# このように、このコードはUserモデルが正しく動作することを確認するためのテストを提供しています。
